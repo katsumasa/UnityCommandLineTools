fede1ec73a0c23b1516c0508f991d96b233c9449
katsumasa <katsumasa@unity3d.com>
2020-09-18 17:01:36 +0900

    Add files via upload

---
+++ Editor/Binary2TextExec.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+namespace UTJ.UnityCommandLineTools
+{
+    // <summary>
+    // bin2textをUnityEditorから実行する為のClass
+    // programed by Katsumasa.Kimura
+    // </summary>
+    public class Binary2TextExec : EditorToolExec
+    {
+        public Binary2TextExec() : base("binary2text*") { }
+
+        // <summary>
+        // bin2text filePath outPath options
+        // /summary>
+        public int Exec(string filePath,string outPath,string options)
+        {
+            var args = string.Format(@"""{0}"" ""{1}"" {2}", filePath, outPath, options);
+            return Exec(args);
+        }
+    }
+}
+#endif
---
+++ Editor/EditorToolExec.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System;
+using UnityEditor;
+using UnityEngine;
+
+#if UNITY_EDITOR
+
+namespace UTJ.UnityCommandLineTools
+{
+    // <summary>
+    // UnityEditorに含まれるコマンドラインツールを実行する為の基底Class
+    // programed by Katsumasa.Kimura
+    //</summary>
+    public class EditorToolExec
+    {
+        // <value>
+        // UnityEditorがインストールされているディレクトリへのパス
+        // </value>
+        protected string mEditorPath;
+
+        // <value>
+        // Toolsディレクトリへのパス
+        // </value>
+        protected string mToolsPath;
+
+        // <value>
+        // 実行ファイル名
+        // </value>
+        protected string mExecFname;
+
+        // <value>
+        // 実行ファイルへのフルパス
+        // </value>
+        protected string mExecFullPath;
+
+        // <value>
+        // 実行結果のOUTPUT
+        // </value>
+        private string mOutput;
+
+        // <value>
+        // 実行結果のOUTPUT
+        // </value>
+        public string output
+        {
+            get { return mOutput; }
+        }
+
+        // <summary>
+        // コンストラクタ
+        // <param>
+        // mExecFname : 実行ファイル名
+        // </param>
+        // /summary>
+        public EditorToolExec(string mExecFname)
+        {
+            mEditorPath = Path.GetDirectoryName(EditorApplication.applicationPath);            
+            mToolsPath = Path.Combine(mEditorPath, @"Data/Tools");
+            this.mExecFname = mExecFname;
+            var files = Directory.GetFiles(mToolsPath, mExecFname, SearchOption.AllDirectories);
+            mExecFullPath = files[0];
+        }
+
+        // <summary> 
+        // コマンドラインツールを実行する
+        // <param> 
+        // arg : コマンドラインツールに渡す引数 
+        // </param>
+        // </summary>
+        public int Exec(string arg)
+        {
+            int exitCode = -1;
+
+            try
+            {
+                using (var process = new Process())
+                {
+                    process.StartInfo.FileName = mExecFullPath;
+                    process.StartInfo.Arguments = arg;
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.RedirectStandardOutput = true;
+                    process.Start();
+                    mOutput = process.StandardOutput.ReadToEnd();
+                    process.WaitForExit();
+                    exitCode = process.ExitCode;
+                    process.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                UnityEngine.Debug.Log(e);
+            }
+            return exitCode;
+        }
+    }
+}
+#endif
